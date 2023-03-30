a4dbec7407d16ebb20d8c9911fe89bcb0a8d5c80
Criquillion <louis.criquillion@gmail.com>
2023-03-30 09:20:48 +0200

    bdd

---
+++ SmartEats.session.sql
@@ -0,0 +1,4 @@
+INSERT INTO utilisateurs (ID, TYPE, MAIL, PASSWORD, NOM, PRENOM, DATENAISSANCE, POIDS, TAILLE_CM, SEXE)
+VALUES (1, 1, 'louis.criquillion@gmail.com', 'password', 'Criquillion', 'Louis', '2002-10-18', 30.1, 180.3, 'H');
+
+  
