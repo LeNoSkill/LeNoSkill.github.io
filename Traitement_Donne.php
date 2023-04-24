29f3b6d1d1b984dc039494fd36c8c43ba224c2bf
Criquillion <louis.criquillion@gmail.com>
2023-04-24 20:56:14 +0200

    INSCRIPTION SESSION

--- Traitement_Donne.php
+++ Traitement_Donne.php
@@ -190,7 +190,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         //$_SESSION['user_id'] = $user['ID']; mit de base dans la bddd
         $_SESSION['user_name'] = $name;
-        //$_SESSION['user_type'] = $user['TYPE']; 0 de base donc ne change rien car de base est jsute client pas admin
+        $_SESSION['user_type'] = 0; //0 de base donc ne change rien car de base est jsute client pas admin
         $_SESSION['user_prenom'] = $firstname;
 
         header("Location: php/test.php");
