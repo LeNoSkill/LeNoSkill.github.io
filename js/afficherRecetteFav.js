23bf5edb0eeafa936324ce79f655b9c6339c0f4c
mateofont <mateofontaine30@gmail.com>
2023-04-27 19:53:33 +0200

    Favoris checkbox

--- js/afficherRecetteFav.js
+++ js/afficherRecetteFav.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
     let i = 0 ;
 // Affiche les recettes et cache le reste
 $("#afficherrecette").click(function() {
@@ -28,6 +28,10 @@ $("#afficherrecette").click(function () {
           $('<h5>').text(obj.nom_recette).addClass('card-title').appendTo(cardBody);
           $('<p>').text('Temps de préparation : ' + obj.temps_preparation).appendTo(cardBody);
           $('<p>').text('Temps de cuisson : ' + obj.temps_cuisson).appendTo(cardBody);
+           // Ajout de la checkbox
+           var fav = $('<input>').attr('type', 'checkbox').attr('id', 'favoriRecette').val(obj.id).appendTo(cardBody);
+           //var checkbox = $('<input>').attr('type', 'checkbox').attr('id', 'favoriRecette').attr('value', obj.id).appendTo(cardBody);
+           $('<label>').text('Ajouter aux favoris').appendTo(cardBody);
 
           var uniqueId = obj.nom_recette.replace(/\s+/g, ''); // remove spaces to create a unique ID
           var collapseButton = $('<button>').addClass('btn btn-primary').attr('type', 'button').attr('data-bs-toggle', 'collapse').attr('data-bs-target', '#collapse' + uniqueId).text('Voir les ingrédients et instructions').appendTo(cardBody);
@@ -56,4 +60,4 @@ $("#afficherrecette").click(function () {
 });
 
 });
-*/
+
