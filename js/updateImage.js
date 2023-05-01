56c8f90474683dc6c8712fe8810b148d006cffa4
mateofont <mateofontaine30@gmail.com>
2023-05-01 21:06:47 +0200

    encore test photo

--- js/updateImage.js
+++ js/updateImage.js
@@ -1,8 +1,12 @@
 
 function updateImageURL() {
+
+  alert('passage fonction');  
     const fileInput = document.getElementById('image_file');
     const imageUrlInput = document.getElementById('image_url');
+
     if (fileInput.files && fileInput.files[0]) {
+      alert('passage boucle');
       const fileName = fileInput.files[0].name;
       imageUrlInput.value = `../img/${fileName}`;
     }
